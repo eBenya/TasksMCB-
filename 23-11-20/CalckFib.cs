fe2ca362e06de02015b6e12511c8f5af36989d45
eBenya <MyMail@com>
2020-11-23 17:35:21 +0300

    Add dz

---
+++ 23-11-20/CalckFib.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace AsyncProgramming
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            int number = 13;
+
+            long result = CalculateFactorial(number);
+
+            Console.WriteLine($"Результат - {result}");
+
+            while (true)
+            {
+                Console.Write("*");
+                Thread.Sleep(300);
+            }
+        }
+
+        private static long CalculateFactorial(int number)
+        {
+            Thread.Sleep(500);
+
+            if (number == 1)
+            {
+                return number;
+            }
+            else
+            {
+                return CalculateFactorial(number - 1) * number;
+            }
+        }
+    }
+}
---
+++ 23-11-20/Task0.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+/**
+ * Создайте  проект  по  шаблону  "ConsoleApplication".  
+ * Создайте  метод  с  названием  WriteChar, 
+ * который принимает один параметр типа char с названием symbol.
+ * В методе необходимо создать цикл for размерностью в 160 итераций,
+ * который будет выводить на экран консоли значение параметра  symbol
+ * с  задержкой  в  100  миллисекунд.  Из  метода  Main,
+ * используя  пул  потоков, организуйте параллельный вывод на экран двух символов
+ * звездочки и знака восклицания.
+ **/
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        const int sleep = 50;
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(WriteChar, '*');
+            ThreadPool.QueueUserWorkItem(WriteChar, '!');
+
+            Console.Read();
+        }
+
+        public static void WriteChar(object symbol)
+        {
+            char c = (char)symbol;
+
+            for (int i = 1; i < 160; i++)
+            {
+                Console.Write(c);
+                Thread.Sleep(sleep);
+            }
+        }
+    }
+}
