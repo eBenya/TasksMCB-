0a573d23da9de4f0ac91c1109b5a68f2e632ad3e
eBenya <MyMail@com>
2020-11-24 23:21:56 +0300

    Add Movies webApp. Create Model, add  RazorPage Entity CRUD template, changed the markup

---
+++ 24-11-20/ViewMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ViewMovie.Models;
+using ViewMovie.Data;
+
+namespace ViewMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ViewMovie.Data.ViewMovieContext _context;
+
+        public IndexModel(ViewMovie.Data.ViewMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
