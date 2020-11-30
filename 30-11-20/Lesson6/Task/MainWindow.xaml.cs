fc03161743f8b627c2a3fd32811f6f87aca77434
eBenya <mail.ru>
2020-11-30 17:20:44 +0300

    Add 30-11-20 Tasks

---
+++ 30-11-20/Lesson6/Task/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_TEST
+{
+    /**
+     * Задание 
+     * Создайте приложение по шаблону WPFApplication. 
+     * Переместите из элементов управления (ToolBox) на форму текстовое поле и кнопку. 
+     * Создайте асинхронный обработчик события по нажатию на кнопку.
+     * Используя класс HttpClient загрузите из Интернета html код любой страницы.
+     * Выведите его в текстовое поле.
+     **/
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButoonEvenHandler(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = false;
+
+            await Task.Run(()=>Thread.Sleep(3000));
+
+            HttpClient httpClient = new HttpClient();
+            string res = await httpClient.GetStringAsync(@"https://zxpress.ru/article.php?id=8482");
+            
+            txtResault.Text = res.ToString();
+            
+            btnStart.IsEnabled = true;
+        }
+    }
+}
---
+++ 30-11-20/Lesson6/Task5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+/**
+ *Задание 5
+ *Создайте приложение по шаблону Console  Application. 
+ *Запросите у пользователя любое число. 
+ *Создайте асинхронную операцию с помощью класса TaskCompletionSource, 
+ *которая в контексте потока из пула, 
+ *считает всю последовательность чисел от 0 до указанного пользователем числа. 
+ *Результат задачи выведите на экран консоли.
+ **/
+
+namespace ConsoleTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountToAsync(15);
+            Console.ReadKey();
+        }
+
+        private static Task CountToAsync(int num)
+        {
+            TaskCompletionSource<int> task = new TaskCompletionSource<int>();
+            Task.Run(async () =>
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    Console.WriteLine(i);
+                    await Task.Delay(100);
+                }                
+            });
+
+            return task.Task;
+        }
+    }
+}
