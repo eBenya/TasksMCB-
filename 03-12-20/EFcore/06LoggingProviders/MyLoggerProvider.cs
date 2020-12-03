79d0c146f55d20a51f971f4c5b1a558e143f6b5a
eBenya <mail.ru>
2020-12-03 17:58:34 +0300

    Add 03-12-20. Loggging provider. Local logging registration

---
+++ 03-12-20/EFcore/06LoggingProviders/MyLoggerProvider.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTEST
+{
+    public class MyLoggerProvider : ILoggerProvider
+    {
+        //Creates and returns a logger object. For create it, use the path to file, that passed throught the constructor.
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new MyLogger();
+        }
+
+        //Manage the reliase of resources.
+        public void Dispose() {}
+
+        private class MyLogger : ILogger
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <typeparam name="TState"></typeparam>
+            /// <param name="state"></param>
+            /// <returns>Returns an IDisposable object that represents some scope for the logger.</returns>
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return null;
+            }
+
+            //Indicatade wather the object is available for use.
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+
+            /// <summary>
+            /// Makes a log. In this implementation write to the file and console.
+            /// </summary>
+            /// <typeparam name="TState">The type of object to write.</typeparam>
+            /// <param name="logLevel">Level of detail of the current message.</param>
+            /// <param name="eventId">Event ID.</param>
+            /// <param name="state">Some state object that store the message.</param>
+            /// <param name="exception">Information about exception.</param>
+            /// <param name="formatter">Formating function, with state and exception parametrs.</param>
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                File.AppendAllText("log.txt", formatter(state, exception));
+                Console.WriteLine(formatter(state, exception));
+            }
+        }
+    }
+
+
+}
