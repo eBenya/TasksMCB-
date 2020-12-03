a8f80619356af999738e86bad7f35c6330915d1a
eBenya <mail.ru>
2020-12-03 16:07:58 +0300

    Add 03-12-20

---
+++ 03-12-20/EFcore/01CreateDb/FirstApp.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+/**
+ * Create and write/read to/from dbase
+ **/
+
+namespace ConsoleTEST
+{
+    class Program
+    {
+        static void Main()
+        {
+            //DbContext realize IDisposable
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User user1 = new User { Name = "Kek", Age = 14 };
+                User user2 = new User { Name = "Puc", Age = 54 };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+
+                db.SaveChanges();
+                Console.WriteLine($"{nameof(user1)} and {nameof(user2)} are successfully saved.");
+
+                var users = db.Users.ToList();
+                Console.WriteLine("Users list:");
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"\tid:{user.Id} - Name:{user.Name} - Age:{user.Age}");
+                }
+            }
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            //Checks whether the DB exist, if not create it
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder oprionBr)
+        {
+            //                        Server engine                        NameDB
+            oprionBr.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=firstUserAppdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ 03-12-20/EFcore/03BasikDataOperations/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleTEST
+{
+    class Program
+    {
+        static void Main()
+        {
+            string ceiling = new string('_', 40);
+
+            #region Add(Create)
+            Console.WriteLine("Create opperation:");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User user1 = new User { Name = "Kek", Age = 17 };
+                User user2 = new User { Name = "Puc", Age = 76 };
+
+                db.Users.AddRange(user1, user2);
+                db.SaveChanges();
+            }
+            Console.WriteLine(ceiling);
+            #endregion
+
+            #region Read
+            Console.WriteLine("Read opperation:");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Users.ToList();
+
+                PrintCollection(users);
+            }
+            Console.WriteLine(ceiling);
+            #endregion
+
+            #region Update
+            Console.WriteLine("Update opperation:");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User user = db.Users.FirstOrDefault();
+                if (user!=null)
+                {
+                    user.Name = "Lol";
+                    user.Age += 5;
+
+                    //Update object
+                    db.Users.Update(user);
+                    //db.SaveChanges();
+                }
+
+                var users = db.Users.ToList();
+
+                PrintCollection(users);
+            }
+            Console.WriteLine(ceiling);
+            #endregion
+
+            #region Delete
+            Console.WriteLine("Delete opperation:");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User user = db.Users.FirstOrDefault(u => u.Name == "Puc");
+                if (user != null)
+                {
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                }
+
+                Console.WriteLine("Delete the Puc from Db:");
+                PrintCollection(db.Users.ToList());
+
+                db.Users.Remove(db.Users.FirstOrDefault());
+                db.SaveChanges();
+
+                Console.WriteLine("Deletung the second user:");
+                PrintCollection(db.Users.ToList());
+            }
+
+            Console.WriteLine(ceiling);
+            #endregion
+
+        }
+
+        static void PrintCollection(IEnumerable<User> users)
+        {
+            foreach (var user in users)
+            {
+                Console.WriteLine($"\t{user.Id}.{user.Name} - {user.Age}");
+            }
+        }
+    }
+}
