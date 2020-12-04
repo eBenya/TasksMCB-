fe02e25f88af2b506f807358b2e24ad433adab72
eBenya <mail.ru>
2020-12-04 19:36:34 +0300

    Add 04-12-20. Foreign-key and Navigation properties. Cascading deletion

---
+++ 04-12-20/EFcore/14ForeginKeyAndNavigationProperties/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleTEST
+{
+    class Program
+    {
+        static void Main()
+        {
+            string ceiling = new string('_', 40);
+
+            StringBuilder sb = new StringBuilder(256);
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Company company1 = new Company { Name = "C1" };
+                Company company2 = new Company { Name = "C2" };
+                User user1 = new User { Name = "Kek", Company = company1 };
+                User user2 = new User { Name = "Puc", Company = company1 };
+                User user3 = new User { Name = "Lol", Company = company2 };
+                db.Users.AddRange(user1, user2, user3);
+                db.Companies.AddRange(company1, company2);
+                db.SaveChanges();
+
+                foreach (var user in db.Users.ToList())
+                {
+                    Console.WriteLine($"{user.Name} работает в {user.Company?.Name}");
+                }
+            }
+
+            Console.WriteLine(sb);
+        }        
+    }
+}
