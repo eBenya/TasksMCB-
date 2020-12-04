7673e51677dc0697b8b2c30a89cdc7c042078f4a
eBenya <mail.ru>
2020-12-04 10:17:38 +0300

    Add 04-12-20. DataBaseMigrationAndManagment. Add change in user model. Add migration

--- 04-12-20/EFcore/07DbMigrationAndManagment/Migrations/20201204071545_UserIsMarriedMigration.Designer.cs
+++ 04-12-20/EFcore/07DbMigrationAndManagment/Migrations/20201204071545_UserIsMarriedMigration.Designer.cs
@@ -3,14 +3,16 @@ using ConsoleTEST;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ConsoleTEST.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20201204071545_UserIsMarriedMigration")]
+    partial class UserIsMarriedMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
