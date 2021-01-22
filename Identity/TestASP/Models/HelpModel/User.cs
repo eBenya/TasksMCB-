2f1b978b29441d947699a36d01a8adbf205281f6
eBenya <mail.ru>
2021-01-22 15:31:38 +0300

    asp.net core identity Initial. Create Readme and project. Realize add user, register, logIn

---
+++ Identity/TestASP/Models/HelpModel/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TestASP.Models.HelpModel
+{
+    public class User : IdentityUser
+    {
+        public int Year { get; set; }
+    }
+
+    public class CreateUserViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Year { get; set; }
+    }
+
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public int Year { get; set; }
+    }
+}
