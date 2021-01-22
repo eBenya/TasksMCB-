e638b673695a8ea584a611fce52453d1d4e59627
eBenya <mail.ru>
2021-01-22 21:00:19 +0300

    add initialization by users and roles

--- Identity/TestASP/Program.cs
+++ Identity/TestASP/Program.cs
@@ -1,19 +1,43 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestASP.Models.HelpModel;
+using TestASP.Settings.Initializing;
 
 namespace TestASP
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                //get container
+                var services = scope.ServiceProvider;
+                try
+                {
+                    //get user and role manager via GetRequiredService
+                    var userManager = services.GetRequiredService<UserManager<User>>();
+                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    await UsersInitializing.InitializeAsync(userManager, rolesManager);
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ Identity/TestASP/Settings/Initializing/UsersInitializing.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestASP.Models.HelpModel;
+
+namespace TestASP.Settings.Initializing
+{
+    public class UsersInitializing
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {            
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+
+            string userEmail = "admin@kek.sec";
+            string password = "admin";
+            if (await userManager.FindByNameAsync(userEmail) == null)
+            {
+                User admin = new User { Email = userEmail, UserName = userEmail };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+
+            userEmail = "user@kek.com";
+            password = "123";
+            if (await userManager.FindByNameAsync(userEmail) == null)
+            {
+                User user = new User { Email = userEmail, UserName = userEmail };
+                IdentityResult result = await userManager.CreateAsync(user, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "admin");
+                }
+            }
+        }
+    }
+}
